2faf82019fe65e35156b65f7efcc5421266bb5d4
Craig Hart <i@craigary.net>
2026-02-18 21:28:08 +0800

    Set output option to 'server' in astro.config.mjs

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -7,6 +7,7 @@ import { podcastConfig } from '@hn/config'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   adapter: cloudflare(),
   site: podcastConfig.site.url,
   image: {
